b21e8466e1a8b4e30f6c2de1339d49488e5f29f3
Enzo Faliveni <enzofali.ef@gmail.com>
2026-05-03 20:16:05 -0300

    fix(prometheus): drop apk (image isn't alpine), template config via sed

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 : "${API_TARGET:=api:8000}"
-export API_TARGET
 
-envsubst < /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
+# Substitute the placeholder with the actual target. sed because envsubst is not
+# in the prom/prometheus image and we don't want to pay the install cost.
+sed "s|__API_TARGET__|${API_TARGET}|g" \
+    /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
 
 exec /bin/prometheus \
   --config.file=/etc/prometheus/prometheus.yml \
